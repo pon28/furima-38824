4ec95611cd10c39f640c15e01bc39b0a6598d7c8
pon28 <rcy.mry.sh.28@gmail.com>
2023-04-20 21:18:46 +0900

    テストコードの修正１

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,7 +66,19 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it "passwordが半角英数字でないと登録できない" do
+      it "passwordが全角文字では登録できない" do
+        @user.password = 'あいうえお'
+        @user.password_confirmation = 'あいうえお'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it "passwordが半角数字では登録できない" do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it "passwordが半角英語では登録できない" do
         @user.password = 'abcdef'
         @user.password_confirmation = 'abcdef'
         @user.valid?
