dfb0e9366faea738fe76aef9245769712f73b484
pon28 <rcy.mry.sh.28@gmail.com>
2023-04-27 17:14:00 +0900

    カテゴリーの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one_attached :image
 
+  belongs_to :category
+
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
   validates :status_id, presence: true
   validates :shipping_cost_id, presence: true 
   validates :prefecture_id, presence: true 
   validates :shipping_day_id, presence: true
   validates :price, presence: true
   validates :user, presence: true
+
 end
