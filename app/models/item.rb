e48ce104d5ec9e3741ffe5706bfbd8feaa04a0a7
pon28 <rcy.mry.sh.28@gmail.com>
2023-04-27 17:48:25 +0900

    発送元の地域のカテゴリー追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,13 +7,14 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :shipping_cost
+  belongs_to :prefecture
 
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
   validates :status_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank"} 
-  validates :prefecture_id, presence: true 
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank"} 
   validates :shipping_day_id, presence: true
   validates :price, presence: true
   validates :user, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_27_082705) do
+ActiveRecord::Schema.define(version: 2023_04_27_084200) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 2023_04_27_082705) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.integer "prefecture_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "shipping_costs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
