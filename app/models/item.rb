46d4aa19e61e182135a3622b1cb67daf82080d2c
pon28 <rcy.mry.sh.28@gmail.com>
2023-04-27 17:25:22 +0900

    商品の状態カテゴリー追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,12 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   belongs_to :category
+  belongs_to :status
 
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :status_id, presence: true
+  validates :status_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
   validates :shipping_cost_id, presence: true 
   validates :prefecture_id, presence: true 
   validates :shipping_day_id, presence: true
