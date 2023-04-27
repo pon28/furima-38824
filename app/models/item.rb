9a7221aada9517c1a1f3c712ee95821bed11cc2d
pon28 <rcy.mry.sh.28@gmail.com>
2023-04-27 17:41:00 +0900

    配送料のカテゴリー追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,13 @@ class Item < ApplicationRecord
 
   belongs_to :category
   belongs_to :status
+  belongs_to :shipping_cost
 
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
   validates :status_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :shipping_cost_id, presence: true 
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank"} 
   validates :prefecture_id, presence: true 
   validates :shipping_day_id, presence: true
   validates :price, presence: true
