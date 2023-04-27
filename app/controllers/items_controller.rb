c91c7f4577c5e08531e64bccfaa20f76b87039f9
pon28 <rcy.mry.sh.28@gmail.com>
2023-04-27 11:22:35 +0900

    itemsテーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id , :shipping_cost_id, :prefecture_id , :shipping_day_id, :price)
+    
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_cost_id, presence: true 
+  validates :prefecture_id, presence: true 
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+end
