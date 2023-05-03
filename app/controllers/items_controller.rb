bc26b4b70bd4a0417389fbbcfcedc75687924b0d
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-03 10:18:10 +0900

    データベースに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,7 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id , :shipping_cost_id, :prefecture_id , :shipping_day_id, :price, :image).merge(user_id: current_user.id)
     
