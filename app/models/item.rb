75ad8e33e7c2055e65b1d66949cbdde557023495
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-03 23:37:41 +0900

    正規表現の削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,6 +18,6 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank"} 
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank"} 
   validates :shipping_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank"} 
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
 end
