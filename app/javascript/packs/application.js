3df676583c4992870866c137ac65d129627975a4
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-03 11:19:56 +0900

    販売手数料の計算

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,13 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ));
+  })
+});
