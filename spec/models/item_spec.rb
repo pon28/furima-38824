d800d71a2e6f177f515a68dbe6d8433aa95deb0e
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-04 18:54:53 +0900

    テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -111,7 +111,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it '販売価格が半角数字以外のときは登録できない' do
-
+        @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'userが紐づいていない場合は登録できない' do
         @item.user = nil
