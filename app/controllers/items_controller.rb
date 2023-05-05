093099b50058614541e80c02043df831c5ed847e
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-05 14:04:57 +0900

    @itemsの定義を一つに修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
     @item = Item.all
-    @items = @item.includes(:user)
-    @items = Item.order("created_at DESC")
+    @items = @item.includes(:user).order("created_at DESC")
   end
 
   def new
