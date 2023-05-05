f40eedb38590f411574158b26c426ede407304e8
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-05 16:42:56 +0900

    不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @item = Item.all
     @items = @item.includes(:user).order("created_at DESC")
   end
 
