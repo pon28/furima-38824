fb7e0d6496080d6db991086af600b45359b22420
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-06 10:15:51 +0900

    コントローラーの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+  end
+
   private
 
   def item_params
