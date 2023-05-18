87d53cf9e8a952938d7e6a2258f05a4e6516578e
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-19 00:29:54 +0900

    orders訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,23 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
-    @order = Order.new
+    if @item.order.present? || current_user.id == @item.user_id
+      redirect_to root_path
+    end
+    @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_address = OrderAddress.new(order_params)
+    # @order = Order.create(order_params)
+      # Address.create(address_params)
+      # if @order.valid?
+    if @order_address.valid?
       pay_item
-      @order.save
-      return redirect_to root_path
+      @order_address.save
+      redirect_to root_path
     else
       render :index
     end
@@ -20,16 +26,25 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:price, :post_code).merge(token: params[:token])
+    # params.require(:order).permit(:price, :post_code).merge(token: params[:token])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
+  # def address_params
+  #   params.permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order).merge(order_id: @order.id)
+  # end
+
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,  
+      card: order_params[:token],    
+      currency: 'jpy'                 
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
