38e2f8737e962118713c930b3cf42d3fff89a4ed
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-19 00:29:37 +0900

    OrderAdress作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,22 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :token, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :token
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: "は半角数字で入力してください" }
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: '電話番号は11桁以内で入力してさい。' }
+    validates :city
+    validates :house_number
+  end
+  
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
