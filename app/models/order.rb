7310cc93973d3ecae7ced57944a1e66e13657d05
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-19 00:28:35 +0900

    アソシエーションの訂正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ApplicationRecord
   attr_accessor :token
-  validates :price, presence: true
-  validates :token, presence: true
+  belongs_to :user
+  belongs_to :item
+  has_one :address
+  # validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  # validates :token, presence: true
 end
