2b16e4e0b13cd1efb005fcfd7df6f663b490fa33
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-20 18:39:55 +0900

    固定電話も許可できるよう10桁以上11桁以下に変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: "は半角数字で入力してください" }
-    validates :phone_number, format: { with: /\A\d{11}\z/, message: '電話番号は11桁以内で入力してさい。' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: '電話番号は11桁以内で入力してさい。' }
     validates :city
     validates :house_number
   end
