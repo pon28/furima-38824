8c7688b59a764831e8176eddd878618bbe8db726
pon28 <rcy.mry.sh.28@gmail.com>
2023-05-21 21:09:32 +0900

    Update 20230427062143_create_categories.rb

--- db/migrate/20230427062143_create_categories.rb
+++ db/migrate/20230427062143_create_categories.rb
@@ -1,8 +1,10 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
-    t.string     :title        , null: false
-    t.text       :text         , null: false
-    t.integer    :category_id     , null: false
-    t.timestamps
+    create_table :categories do |t|
+      t.string     :title        , null: false
+      t.text       :text         , null: false
+      t.integer    :category_id     , null: false
+      t.timestamps
+    end
   end
 end
